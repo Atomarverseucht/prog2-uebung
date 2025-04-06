7fdb4b09d4a00e7a3f798b36801755f4c10c023e
Herbert-Haase <herberthaase02@gmx.de>
2025-04-06 16:16:43 +0200

    add add method

--- src/aufgabe2/LinkedListFrequencyTable.java
+++ src/aufgabe2/LinkedListFrequencyTable.java
@@ -8,33 +8,34 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
     private Node begin;
     private Node end;
 
-    public class Node{
+    public class Node {
         private Node previous;
         private Node next;
 
         private Word data;
 
-        Node(Node n, Node p, Word w){
-          next = n;
-          previous = p;
-          data = w.copy();
+        Node(Node n, Node p, Word w) {
+            next = n;
+            previous = p;
+            data = w.copy();
         }
-        Word data(){
+
+        Word data() {
             return new Word(data.getWord(), data.getFrequency());
         }
 
-        void addFrequency(int f){
+        void addFrequency(int f) {
             data.addFrequency(f);
         }
     }
 
     @Override
-    public int size(){
+    public int size() {
         return size;
     }
 
     @Override
-    public void clear(){
+    public void clear() {
         begin = new Node(null, null, null);
         end = new Node(null, null, begin);
         begin.next = end;
@@ -42,16 +43,38 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
     }
 
     @Override
-    public void add(String w, int f){
+    public void add(String w, int f) {
+        if (f <= 0) {
+            throw new IllegalArgumentException();
+        }
+        Node p = begin;
+        for (int i = 0; i < size; i++) {
+            p = p.next;
+            if (p.data.getWord().equals(w)) {
+                p.data.addFrequency(f);
+                moveToLeft(p);
+                return;
+            }
+        }
+        Node pre = end.previous;
+        Word newWord = new Word(w, f);
+        Node newNode = new Node(pre, end, newWord);
+        pre.next = newNode;
+        end.previous = newNode;
+        moveToLeft(p);
+        size++;
+    }
+    public void moveToLeft(Node p) {
+
     }
 
     @Override
-    public int get(String f){
+    public int get(String f) {
         return 0;
     }
 
     @Override
-    public Word get(int pos){
+    public Word get(int pos) {
         return begin.data();
     }
 }
